4fa7638553f26ae72217d634b836106cf8a904db
as2340 <77592730+as2340@users.noreply.github.com>
2022-07-26 18:37:55 +0100

    add validity check for user input

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -34,29 +34,34 @@ rock_paper_scissors = [rock, paper, scissors]
 
 # user input is the index of the list of ascii strings
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
-# print out user's ascii art
-print(rock_paper_scissors[user_choice])
 
-# select random integer for computer's choice
-computer_choice = random.randint(0,2)
-print("Computer chose:")
-print(rock_paper_scissors[computer_choice])
+# check validity of input
+if user_choice < 0 or user_choice > 2:
+    print("Invlaid input, you lose!")
+else:
+    # print out user's ascii art
+    print(rock_paper_scissors[user_choice])
 
-# logic to determine winner
-if user_choice == computer_choice:
-    print("Draw")
-elif user_choice == 0: # rock
-    if computer_choice == 1: # paper
-        print("You lose")
-    else:
-        print("You win!")
-elif user_choice == 1: # paper
-    if computer_choice == 2: # scissors
-        print("You lose")
-    else:
-        print("You win!")
-else: # user choice is 2 - scissors
-    if computer_choice == 0: # rock
-        print("You lose")
-    else:
-        print("You win!")
+    # select random integer for computer's choice
+    computer_choice = random.randint(0,2)
+    print("Computer chose:")
+    print(rock_paper_scissors[computer_choice])
+
+    # logic to determine winner
+    if user_choice == computer_choice:
+        print("Draw")
+    elif user_choice == 0: # rock
+        if computer_choice == 1: # paper
+            print("You lose")
+        else:
+            print("You win!")
+    elif user_choice == 1: # paper
+        if computer_choice == 2: # scissors
+            print("You lose")
+        else:
+            print("You win!")
+    else: # user choice is 2 - scissors
+        if computer_choice == 0: # rock
+            print("You lose")
+        else:
+            print("You win!")
